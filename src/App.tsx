bb1a4b76db50538414906f2bf7734ae1aae3d301
camsandvl <san24358@uvg.edu.gt>
2026-05-18 21:53:57 -0600

    chore: setup Vite + React + Typescript + Vitest + RTL

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Password Strength Meter</h1>
+      <p>Configuración lista. Los tests y el componente se agregarán en las siguientes fases.</p>
+    </div>
+  );
+}
+
+export default App;
